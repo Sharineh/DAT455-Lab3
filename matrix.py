e68920f0a2e6d68f33420b11acd989543e56f0bf
sharineh <sharineh@student.chalmers.se>
2023-07-10 18:19:44 +0200

    Add functions matmul and invert. Both are working fine

--- matrix.py
+++ matrix.py
@@ -27,19 +27,71 @@ def powers(lst,lower_pow,upper_pow):
             
     return power_lst
 
+def matmul(m1,m2):
+    
+    m1_rows = len(m1)
+    
+    m2_cols = len(m2[0])
+    
+    m2_rows = len(m2)
+    
+    
+    result = [[0] * m2_cols for _ in range(m1_rows)]
+    
+    for i in range(m1_rows):
+        
+        for j in range(m2_cols):
+            
+            for k in range(m2_rows):
+                
+                result[i][j] += m1[i][k] * m2[k][j]
+                
+    return result
+
+def invert(M):
+    
+    if len(M) != 2 and len(M[0]) != 2:
+        
+        return "The input should be a 2x2 matrix.\n"
+    
+    else:
+        det_m = M[0][0] * M[1][1] - M[0][1] * M[1][0]
+        
+        inverted_M = [[M[1][1]/det_m, -M[0][1]/det_m],
+                      [-M[1][0]/det_m, M[0][0]/det_m]]
+        
+        return inverted_M
+        
+        
+        
+                
+                
+    
+
 def main():
     
-    matrix = [[1,2,3,4],
-              [5,6,7,8],
-              [9,10,11,12]]
+    matrix = [[7,8,9,10],
+              [11,12,13,14],
+              [15,16,17,18]]
     
-    np_matrix = np.array([[1,2,3,4],
-              [5,6,7,8],
-              [9,10,11,12]])
+    power_lst = [[1,2,3],[4,5,6],[7,8,9]]
     
-    power_lst = [1,2,3,4]
+    #print(powers(power_lst,2,4))
     
-    print(powers(power_lst,2,4))
+    #print(matmul(power_lst,matrix))
+    
+    I3 =[[1,0,0],
+         [0,1,0],
+         [0,0,1]]
+    
+    #print(matmul(power_lst,I3))
+    
+    A = [[1,2],
+         [3,4]]
+    
+    A_inv = invert(A) 
+    
+    print(matmul(A,A_inv))
     
     
 main()
